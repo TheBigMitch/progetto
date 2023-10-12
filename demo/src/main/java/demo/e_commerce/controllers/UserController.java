d1d989bb2b36299930e6dc1d13253e4e0505373c
TheBigMitch <147408888+TheBigMitch@users.noreply.github.com>
2023-10-12 18:25:23 +0200

    prova annotazioni

--- demo/src/main/java/demo/e_commerce/controllers/UserController.java
+++ demo/src/main/java/demo/e_commerce/controllers/UserController.java
@@ -1,5 +1,8 @@
 package demo.e_commerce.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
     public String test()
     {
