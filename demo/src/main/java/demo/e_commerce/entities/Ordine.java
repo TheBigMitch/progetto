bfffffd985fa3662ecf8ac654e92b55a9434c843
TheBigMitch <147408888+TheBigMitch@users.noreply.github.com>
2023-10-13 18:14:11 +0200

    Entities

---
+++ demo/src/main/java/demo/e_commerce/entities/Ordine.java
@@ -0,0 +1,28 @@
+package demo.e_commerce.entities;
+
+import javax.persistence.*;
+import lombok.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="ordini")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Ordine 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Basic
+    private Date data;
+
+    @ManyToOne
+    @JoinColumn(name = "utente")
+    private Utente utente;
+
+    @OneToMany(mappedBy = "ordine", cascade = CascadeType.MERGE)
+    private List<ProdottoOrdinato> prodottiOrdinati;
+}
---
+++ demo/src/main/java/demo/e_commerce/entities/Prodotto.java
@@ -0,0 +1,62 @@
+package demo.e_commerce.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@EqualsAndHashCode
+@Entity
+@Table(name="prodotti")
+public class Prodotto 
+{
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long isbn;
+
+    @Setter
+    @Getter
+    @ManyToOne(fetch= FetchType.LAZY, optional = false)
+    @JsonIgnore
+    @JoinColumn(name="id_categoria", nullable = false)
+    Categoria categoria;
+
+    @Setter
+    @Getter
+    @Basic
+    @Column(name="nome", nullable = false)
+    private String nome;
+
+    @Setter
+    @Getter
+    @Basic
+    @Column(name="immagine", nullable = true)
+    private String UrlImmagine;
+
+    @Basic
+    @Setter
+    @Getter
+    @Column(name="prezzo", nullable = false)
+    private double prezzo;
+
+    @Basic
+    @Setter
+    @Getter
+    @Column(name="descrizione", nullable = true)
+    private String descrizione;
+
+    @Basic
+    @Setter
+    @Getter
+    @Column(name="quantità", nullable = false)
+    private int quantita;
+
+    @Basic
+    @Setter
+    @Getter
+    private String idC;
+}
